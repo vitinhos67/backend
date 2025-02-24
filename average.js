abf0a4f70a9b5b73996d20f57379220a0e440888
Victor Santos da Silva <santtosvictor42@gmail.com>
2025-02-24 20:46:09 -0300

    removed .js

--- average.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function avarage(numbers) {
-    return numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
-}
-console.log(avarage([1, 2, 3, 4, 5]));
-console.log(avarage([2, 2]));
--- concat.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function concatArray(...receiveArray) {
-    return receiveArray.reduce((acc, val) => acc.concat(val), []);
-}
-console.log(concatArray([1, 2343], [45, 78]));
